2ac58a520615d62f0cfe95cb5d3391845aadf050
Akbar <akbarali1klr@gmail.com>
2019-11-25 00:27:56 +0530

    4.2 Blog list, step2

--- app.js
+++ app.js
@@ -1,44 +1,25 @@
-const http = require('http')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb+srv://akbar:nJkcLrkVVyKxxiqA@cluster0-qspqu.mongodb.net/bloglist?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl, { useNewUrlParser: true })
-
+const blogRoutes = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const config = require('./utils/config')
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+    .then(() => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connection to MongoDB:', error.message)
+    })
 app.use(cors())
+app.use(express.static('build'))
 app.use(bodyParser.json())
+app.use(middleware.requestLogger)
+app.use('/api/blogs', blogRoutes);
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+module.exports = app
