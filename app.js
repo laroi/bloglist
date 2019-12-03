2b6b8038e2d7cdac3358a28f62ec2c020749fe60
Akbar <akbarali1klr@gmail.com>
2019-12-03 23:27:09 +0530

    4.18: bloglist expansion, step7

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const blogRoutes = require('./controllers/blogs');
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
@@ -18,8 +19,10 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
 app.use(cors());
 app.use(express.static('build'));
 app.use(bodyParser.json());
-app.use('/api/blogs', blogRoutes);
+app.use(middleware.getTokenFrom);
+app.use('/api/blogs', middleware.verifyToken, blogRoutes);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
--- utils/middleware.js
+++ utils/middleware.js
@@ -6,17 +6,33 @@ const requestLogger = (request, response, next) => {
     console.log('---');
     next();
 };
-
+const jwt = require('jsonwebtoken');
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' });
 };
 
+const getTokenFrom = (request, response, next) => {
+    const authorization = request.get('authorization');
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        request.token = authorization.substring(7);
+    } 
+    next();
+}
+const verifyToken = (req, res, next) => {
+    const decodedToken = jwt.verify(req.token, process.env.SECRET);
+    if (!req.token || !decodedToken.id) {
+       next({name: 'JsonWebTokenError'}); 
+    }
+    next();
+}
 const errorHandler = (error, request, response, next) => {
     logger.error(error.message);
     if (error.name === 'CastError' && error.kind === 'ObjectId') {
         return response.status(400).send({ error: 'malformatted id' });
     } else if (error.name === 'ValidationError') {
         return response.status(400).json({ error: error.message });
+    } else if (error.name === 'JsonWebTokenError') {
+        return response.status(401).json({ error: 'invalid token' })
     }
     next(error);
 };
@@ -24,5 +40,7 @@ const errorHandler = (error, request, response, next) => {
 module.exports = {
     requestLogger,
     unknownEndpoint,
-    errorHandler
+    errorHandler,
+    getTokenFrom,
+    verifyToken,
 };
